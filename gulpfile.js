ed2c015200480d8c2d84a7deed648f70dc347456
jacktho <jack@viosolutions.net>
2016-05-23 13:53:33 -0400

    browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var browserSync = require('browser-sync').create();
 // create a task that ensures the `js` task is complete before
 // reloading browsers
 gulp.task('reload-browser', function(){
-    browserSync.reload('index.html');
+    console.log(browserSync.reload());
 });
 
 // use default task to launch Browsersync and watch JS files
@@ -13,11 +13,13 @@ gulp.task('serve', function () {
     // Serve files from the root of this project
     browserSync.init({
         server: {
-            baseDir: "./"
+          baseDir: "./",
+          reloadDelay: 2000,
         }
     });
 
     // add browserSync.reload to the tasks array to make
     // all browsers reload after tasks are complete.
-    // gulp.watch("js/*.js", ['reload-browser']);
+    // gulp.watch("*.js", ['reload-browser']);
+  gulp.watch("*.html").on("change", browserSync.reload);
 });
