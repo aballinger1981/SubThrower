97b17beed674b47838f02bfd15466a501121a822
aballinger1981 <andrew@andrewballinger.com>
2016-06-06 22:24:42 -0400

    broke the view

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 (function () {
 
-  var app = angular.module("SubThrower", ['ngMaterial', 'ngMessages', 'ngAria', 'ngAnimate', 'ngRoute']);
+  var app = angular.module("SubThrower", ['ngMaterial', 'ngMessages', 'ngAria', 'ngAnimate', 'ngRoute'])
 
+  .config(function($routeProvider) {
+    $routeProvider
+    .when('/currentWeather', {
+      templateUrl: 'currentWeather/view.html',
+      controller: 'subThrowController'
+    })
+    .when('/temp', {
+      templateUrl: 'temp/view.html',
+      controller: 'subThrowController'
+    })
+  });
 
 })();
