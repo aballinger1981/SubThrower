72748bfe3eadf47a9ff70048c90952a5f83cc73b
aballinger1981 <andrew@andrewballinger.com>
2016-06-07 16:35:26 -0400

    broke the app some more

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 
   var app = angular.module("SubThrower", ['ngMaterial', 'ngMessages', 'ngAria', 'ngAnimate', 'ngRoute'])
 
-  .config(function($routeProvider) {
+  .config(function($routeProvider, $locationProvider) {
     $routeProvider
-    .when('/currentWeather', {
-      templateUrl: 'currentWeather/view.html',
-      controller: 'subThrowController'
-    })
-    .when('/temp', {
-      templateUrl: 'temp/view.html',
-      controller: 'subThrowController'
-    })
+      .when('/currentWeather', {
+        templateUrl: 'currentWeather/view.html',
+        controller: 'subThrowController'
+      })
+      .when('/temp', {
+        templateUrl: 'temp/view.html',
+        controller: 'subThrowController'
+      });
+    // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
   });
 
 })();
